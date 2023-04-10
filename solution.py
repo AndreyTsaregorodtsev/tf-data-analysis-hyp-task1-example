8373242fb38765738c93adf038478ae6414a7833
AndreyTsaregorodtsev <71283873+AndreyTsaregorodtsev@users.noreply.github.com>
2023-04-10 23:03:10 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -10,7 +10,7 @@ def solution(x_success: int,
              y_cnt: int) -> bool:
     p1 = x_success/x_cnt
     p2 = y_success/y_cnt
-    if(p1-p2>0.09):
+    if(abs(p1-p2)<0.09):
       return True
     else:
       return False
